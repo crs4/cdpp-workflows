2042ef8da1f705d8023d02c01d30d24bbda641a4
simleo <simleo@crs4.it>
2025-03-26 17:23:45 +0100

    update scripts/prov_crate/tests/run.sh

--- scripts/prov_crate/tests/run.sh
+++ scripts/prov_crate/tests/run.sh
@@ -2,13 +2,9 @@
 set -eoux
 
 pip install -r ../requirements.txt
+pip install roc-validator
 python ../gen_crate.py data -o out
 
-[[ -f out/predictions.cwl ]]
-[[ -f out/ro-crate-metadata.json ]]
-[[ -f out/tissue_high.zip ]]
-[[ -f out/tumor.zip ]]
-cmp <(cat expected_output/ro-crate-metadata.json | jq  'del(."@graph"[0].datePublished, ."@graph"[0].mentions, ."@graph"[7].object, ."@graph"[7].result) | del(."@graph"[]."@id")') <(cat out/ro-crate-metadata.json | jq  'del( ."@graph"[0].datePublished, ."@graph"[0].mentions,."@graph"[7].object, ."@graph"[7].result) | del(."@graph"[]."@id")')
+rocrate-validator -y validate -l REQUIRED -p workflow-run-crate out
 
 rm -r out
-
