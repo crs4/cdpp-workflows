29f122376b2ab4bb2c8850c765772aee1d137f2d
Luca Lianas <luca.lianas@crs4.it>
2025-05-07 11:51:51 +0200

    refactor(scripts/create_user): no need to add the automation default user to the ROIS_MANAGERS group

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -9,6 +9,3 @@ password = os.environ["PROMORT_PASSWORD"]
 user = User.objects.get_or_create(username=username)[0]
 user.set_password(password)
 user.save()
-
-rois_manager_group = Group.objects.get(name="ROIS_MANAGERS")
-rois_manager_group.user_set.add(user)
